fa85d706383868dd98e178d69c2861ad79dbe998
yukin01 <bump1105@gmail.com>
2018-05-05 18:47:55 +0900

    commit

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  int a = 10;
-  int b = 20;
-  int c = a + b;
-
-  cout << c << endl;
-  return 0;
+int main() {
+  cout << 2 << " + " << 3 << " = " << 2 + 3 << endl;
+  cout << "π = " << 3.14159 << "..." << endl;
 }
