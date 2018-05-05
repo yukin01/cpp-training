d9f437a764527ac4f200e11033714892a7d97e1d
yukin01 <bump1105@gmail.com>
2018-05-05 19:27:36 +0900

    commit

--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  cout << 2 << " + " << 3 << " = " << 2 + 3 << endl;
-  cout << "π = " << 3.14159 << "..." << endl;
+int add(int a, int b) {
+  return a + b;
 }
+
+void show(int a, int b) {
+  cout << a << " + " << b << " = " << add(a, b) << endl;
+}
+
+void showPi() {
+  double pi = 3.14159;
+  cout << "π = " << pi << "..." << endl;
+}
+
+int main() {
+  show(3, 4);
+  showPi();
+}
+
